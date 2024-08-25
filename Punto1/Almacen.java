7079304fea577237262e825334088aa8bcb56bc3
andresjarv <andresjarv95@gmail.com>
2024-08-25 12:45:22 -0500

    Actualizacion de punto 1

--- Punto1/Almacen.java
+++ Punto1/Almacen.java
@@ -1,12 +1,86 @@
+import java.util.*;
+
 public class Almacen{
+    Scanner leer = new Scanner(System.in);
     String nombre;
     double precio;
-    int cantidad;
+    int cantidadProducto;
 
     public Almacen(){
         nombre = "";
         precio = 0;
-        cantidad = 0;
+        cantidadProducto = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidadProducto;
+    }
+
+    public void setCantidad(int cantidadProducto) {
+        this.cantidadProducto = cantidadProducto;
+    }
+
+    public Almacen[][] llenarMatrizObjetual(int cantidad){
+        Almacen[][] datoAlmacen = new Almacen[cantidad][cantidad];//Con esta instruccion se declara la matriz        
+        for (int i = 0; i < cantidad; i++) {
+            for (int j = 0; j < cantidad; j++) {
+                Almacen almacen = new Almacen();
+                System.out.println("Ingrese nombre del producto: ");
+                almacen.setNombre(leer.next());
+                System.out.println("Ingrese el precio: ");
+                almacen.setPrecio(leer.nextDouble());
+                System.out.println("Ingrese la cantidad del producto: ");
+                almacen.setCantidad(leer.nextInt());
+                datoAlmacen[i][j]= almacen;
+            }
+            
+        }
+        return datoAlmacen;
+    }
+
+    public void mostrarMatrizObjetual(Almacen[][] datoAlmacen){
+        for (int i = 0; i < datoAlmacen.length; i++) {
+            for (int j = 0; j < datoAlmacen.length; j++) {
+                System.out.print("--"+ datoAlmacen[i][j].getNombre());
+                System.out.print("--"+ datoAlmacen[i][j].getPrecio());
+                System.out.print("--"+ datoAlmacen[i][j].getCantidad());
+            }
+            System.out.println(" ");            
+        }
+    }
+
+    public void BuscarDato(String buscarNombre, Almacen[][] datoAlmacen){
+        boolean encontrado = false;
+        for (int i = 0; i < datoAlmacen.length; i++) {
+            for (int j = 0; j < datoAlmacen.length; j++) {
+                if(datoAlmacen[i][j].getNombre() == buscarNombre){
+                    System.out.println("Valor encontrador en la posicion: ["+i+"]["+j+"]");
+                    encontrado = true;
+                    break;
+                }
+                
+            }
+            if(encontrado)break;
+        }
+        if (!encontrado){
+            System.out.println("Producto no esta en la matriz.");
+        }
     }
 
 }
--- Punto1/Main.java
+++ Punto1/Main.java
@@ -1,7 +1,41 @@
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Almacen almacen = new Almacen();
+        Scanner leer = new Scanner(System.in);
+        boolean flag = true;
+        int cantidad = 0, opc;
+        String nombreBuscar;
+        System.out.println("ingrese el tamano de la matriz objeto: ");
+        cantidad = leer.nextInt();
 
-        System.out.println("Hello World");
+        Almacen[][] datoAlmacen = new Almacen[cantidad][cantidad];
+        while (flag) {
+            System.out.println(
+                    "--Opcion a realizar--\n1.Llenar Matriz Objetual.\n2.Mostrar matriz de objetos.\n3.Buscar un dato por el nombre.\n4.Salir");
+            opc = leer.nextInt();
+            switch (opc) {
+                case 1:
+                    datoAlmacen = almacen.llenarMatrizObjetual(cantidad);
+                    break;
+                case 2:
+                    almacen.mostrarMatrizObjetual(datoAlmacen);
+                    break;
+                case 3:
+                    System.out.println("Buscar un dato por el nombre: ");
+                    nombreBuscar = leer.nextLine();
+                    almacen.BuscarDato(nombreBuscar, datoAlmacen);
+                    break;
+                case 4:
+                    flag = false;
+                default:
+                    System.out.println("Opcion no es valida ");
+                    break;
+            }
+        }
+
     }
-    
+
 }
