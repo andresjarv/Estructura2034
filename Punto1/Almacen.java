2c448667d75d858556d64194aed75edd95a3f2bc
andresjarv95 <andresjarv95@gmail.com>
2024-08-25 09:26:01 -0500

    punto1 taller preparatorio

---
+++ Punto1/Almacen.java
@@ -0,0 +1,12 @@
+public class Almacen{
+    String nombre;
+    double precio;
+    int cantidad;
+
+    public Almacen(){
+        nombre = "";
+        precio = 0;
+        cantidad = 0;
+    }
+
+}
