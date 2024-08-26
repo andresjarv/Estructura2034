bffdc551e798d110fb7242a9e4589a8e71279356
sis8 <sis8@confesas.local>
2024-08-26 15:58:59 -0500

    punto 3 de libreria

---
+++ Punto3/Libro.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Libro{
+
+    Scanner leer = new Scanner(System.in);
+    String titulo;
+    String autor;
+    Double precio;
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public Double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    
+    public Libro[][] llenarLibros(int cantidad){
+        Libro[][] datoLibro = new Libro[cantidad][cantidad];//Con esta instruccion se declara la matriz        
+        for (int i = 0; i < cantidad; i++) {
+            for (int j = 0; j < cantidad; j++) {
+                Libro libro = new Libro();
+                System.out.println("Ingrese titulo del libro: ");
+                libro.setTitulo(leer.next());
+                System.out.println("Ingrese el autor: ");
+                libro.setAutor(leer.next());
+                System.out.println("Ingrese el precio: ");
+                libro.setPrecio(leer.nextDouble());
+                datoLibro[i][j]= libro;
+            }
+            
+        }
+        return datoLibro;
+    }
+
+    public void mostrarLibros(Libro[][] datoLibro){
+        for (int i = 0; i < datoLibro.length; i++) {
+            for (int j = 0; j < datoLibro.length; j++) {
+                System.out.print("[ "+ datoLibro[i][j].getTitulo());
+                System.out.print(" - "+ datoLibro[i][j].getAutor());
+                System.out.print(" - "+ datoLibro[i][j].getPrecio()+" ]");
+            }
+            System.out.println(" ");            
+        }
+    }
+
+}
---
+++ Punto3/MenuLibro.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class MenuLibro {
+    public static void main(String[] args) {
+        Libro libro = new Libro();
+        Scanner leer = new Scanner(System.in);        
+        int cantidad = 0;
+        //String nombreBuscar;
+        System.out.println("ingrese el tamaño de los datos a almacenar: ");
+        cantidad = leer.nextInt();
+
+        Libro[][] datoLibro = new Libro[cantidad][cantidad];
+
+        datoLibro = libro.llenarLibros(cantidad);
+        libro.mostrarLibros(datoLibro);
+
+    }
+
+}
