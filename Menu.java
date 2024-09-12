ffae65406ae8b0c48b5a3bd33cb3302aa1182273
sis8 <sis8@confesas.local>
2024-09-12 11:00:48 -0500

    menu para probar los ejercicios

--- Menu.java
+++ Menu.java
@@ -6,34 +6,72 @@ import PUNTOS.Punto4;
 public class Menu {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        int cantidad=0;
+        int cantidad = 0;
         System.out.println("ingrese el tamano de la matriz objeto: ");
         cantidad = leer.nextInt();
+        boolean flag = true;
+        int opc = 0;
 
-        //Datos para probar
-        Punto4 teatro = new Punto4();
-        Punto4[][] teatroMatriz = new Punto4[cantidad][cantidad];
+        while (flag) {
+            System.out.println(
+                    "--Opcion a realizar--\n1.Llenar Matriz Objetual.\n2.Mostrar matriz de objetos.\n3.Buscar un dato por el nombre.\n4.Inventario.\n21.Salir.");
+            opc = leer.nextInt();
+            switch (opc) {
+                case 1:
 
-        teatroMatriz = teatro.llenarTeatro(cantidad);
+                    break;
+                case 2:
 
-        for (int i = 0; i < teatroMatriz.length; i++) {
-            for (int j = 0; j < teatroMatriz.length; j++) {
-                System.out.print("[ "+ teatroMatriz[i][j].getFila());
-                System.out.print(" - "+ teatroMatriz[i][j].getNumero());
-                System.out.print(" - "+ teatroMatriz[i][j].getPrecio()+" ]");
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                case 20:
+                    break;
+                case 21:
+                    flag = false;
+                    break;
+
+                default:
+                    System.out.println("Opcion no es valida ");
+                    break;
             }
         }
 
-        matriz.sort(teatroMatriz.getPrecio);
-
-        for (int i = 0; i < teatroMatriz.length; i++) {
-            for (int j = 0; j < teatroMatriz.length; j++) {
-                System.out.println("MATRIZ ORDENADA POR PRECIO");
-                System.out.print("[ "+ teatroMatriz[i][j].getFila());
-                System.out.print(" - "+ teatroMatriz[i][j].getNumero());
-                System.out.print(" - "+ teatroMatriz[i][j].getPrecio()+" ]");
-            }
-        }
     }
-    
+
 }
